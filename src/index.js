637246f1fb39ed55156be4d09a92ae611fff8192
Begemottt <55757703+Begemottt@users.noreply.github.com>
2021-06-17 16:34:21 -0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import * as $ from 'jquery';
 import '../scss/styles.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import Slider from "./components/Slider";
 
-
+const showcase_root = document.getElementById("showcase_wrapper");
+ReactDOM.render(
+    <React.StrictMode>
+        <Slider />
+    </React.StrictMode>,
+    showcase_root
+)
 
 // A bit of code I found on stackoverflow that makes buttons unfocus after you press them. Handy!
 document.addEventListener('click', function(e) { if(document.activeElement.toString() == '[object HTMLButtonElement]'){ document.activeElement.blur(); } });
